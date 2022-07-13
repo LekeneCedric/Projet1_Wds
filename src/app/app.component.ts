c13f4fbde0a8a6e8e48590d00bd4bf89aaa0b997
LekeneCedric <93648451+LekeneCedric@users.noreply.github.com>
2022-07-13 09:16:27 -0400

    initial setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'projet1_wds';
+  constructor(private http:HttpClient){
+
+  }
+  ngOnInit(): void {
+      this.http.get<any>("http://backend.pharmcogroup.net/api/habitations").subscribe(
+        res =>{
+          console.log(res);
+        }
+      )
+  }
+}
