37478664abb0639013848012e8a0a9a93066910a
LekeneCedric <93648451+LekeneCedric@users.noreply.github.com>
2022-07-14 12:17:10 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,26 +9,6 @@ import { ApiService } from './services/api.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'projet1_wds';
-  habitation : IHabitation = 
-  {
-      id: 32,
-      intitule: "test,test,test",
-      created_at: new Date("2022-07-13T18:37:47.000000Z"),
-      updated_at: new Date("2022-07-13T18:37:47.000000Z")
-  }
-  constructor(private http:HttpClient,private api:ApiService){
-
-  }
- async ngOnInit(){
-  //await this.api.deleteHabitation(this.habitation).subscribe(data=>{ console.log(data);} ,error=>{console.log(`Erreur: ${error.message}`)})
-    //  await this.api.getHabitations().subscribe(data=>{
-    //     console.log(data);
-    //   });
-    // await  this.api.postHabitation(this.habitation).subscribe(data=>{ console.log(data); });
-    //  await this.api.getHabitations().subscribe(data=>{
-    //     console.log(data);
-    //   });
-
-  }
+   ngOninit() {
+   }
 }
