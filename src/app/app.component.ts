cc668f60237216277ef05ac8e78cfc82da883b92
LekeneCedric <93648451+LekeneCedric@users.noreply.github.com>
2022-07-14 12:28:48 -0400

    revert changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import IHabitation from './models/habitation.models';
+import { ApiService } from './services/api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+   ngOninit() {
+   }
+}
