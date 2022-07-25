a5277ea3dd56da3ea5121a43157f71979dbee96f
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2022-07-25 15:26:41 +0100

    Delete src directory

--- src/app/models/abonnement_historique.ts
+++
@@ -1,6 +0,0 @@
-export default interface Iabonnement_historique{
-    titre: string,
-    duree: string,
-    periode: Date,
-    couleur: string
-}
--- src/app/models/conseils.models.ts
+++
@@ -1,7 +0,0 @@
-export default interface Iconseil{
-    id: number;
-    couverture: string;
-    titre: string;
-    description: string;
-    semidescription: string;
-}
